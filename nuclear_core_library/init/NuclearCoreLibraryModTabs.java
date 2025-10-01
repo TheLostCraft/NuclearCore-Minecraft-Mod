26382ff651a4e7363a1a2a0cf248bbbcac1cf19a
TheLostCraft <leo.Lorre@gmx.de>
2025-10-01 19:54:55 +0200

    Add files via upload

---
+++ nuclear_core_library/init/NuclearCoreLibraryModTabs.java
@@ -0,0 +1,40 @@
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package nuclear_core_library.init;
+
+import nuclear_core_library.NuclearCoreLibraryMod;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.Registries;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class NuclearCoreLibraryModTabs {
+	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, NuclearCoreLibraryMod.MODID);
+	public static final RegistryObject<CreativeModeTab> NUCLEAR_CORE = REGISTRY.register("nuclear_core",
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.nuclear_core_library.nuclear_core")).icon(() -> new ItemStack(NuclearCoreLibraryModBlocks.RAW_URANIUM_BLOCK.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(NuclearCoreLibraryModBlocks.URAN_ORE.get().asItem());
+				tabData.accept(NuclearCoreLibraryModBlocks.DEEPSLATE_URAN_ORE.get().asItem());
+				tabData.accept(NuclearCoreLibraryModItems.RAW_URANIUM.get());
+				tabData.accept(NuclearCoreLibraryModBlocks.RAW_URANIUM_BLOCK.get().asItem());
+			}).build());
+
+	@SubscribeEvent
+	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
+		if (tabData.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+			tabData.accept(NuclearCoreLibraryModBlocks.URAN_ORE.get().asItem());
+			tabData.accept(NuclearCoreLibraryModBlocks.DEEPSLATE_URAN_ORE.get().asItem());
+		} else if (tabData.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+			tabData.accept(NuclearCoreLibraryModItems.RAW_URANIUM.get());
+		}
+	}
+}
